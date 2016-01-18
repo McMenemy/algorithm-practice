c6d9db03eeef348d457feb40bf87858f40f3a5d5
mcmenemy <mr_mcmenemy@yahoo.com>
2016-01-18 20:40:59 +0000

    push cloud9 workspace

---
+++ hello-cpp-world.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int insertion_sort(int [], int length);
+
+int main() {
+    cout << "Hello World!!!\n";
+    
+    int array[4] = {2, 1, 4, 3};
+    int length = sizeof(array) / sizeof(array[0]);
+    insertion_sort(array, length);
+}
+
+int insertion_sort(int array[], int length) {
+    cout << "sort array here\n";
+    int sorted_array[length];
+
+    for(int i = 1; i < length; i++) {
+        int current = array[i];
+        
+        int i2 = i - 1
+        while(i2 >= 0 && current < array[i2]) {
+            int array[] = array[i]
+            
+        }
+    }
+}
