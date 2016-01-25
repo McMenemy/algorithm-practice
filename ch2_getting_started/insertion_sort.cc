b3c08cb43df33e3a5ab2256fa108dd1056600403
mcmenemy <mr_mcmenemy@yahoo.com>
2016-01-25 21:10:59 +0000

    restructure and clean workspace directory

---
+++ ch2_getting_started/insertion_sort.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// int insertion_sort(int [], int length);
+
+int main() {
+    cout << "Hello World!!!\n";
+    
+    int array[4] = {2, 1, 4, 3};
+    int length = sizeof(array) / sizeof(array[0]);
+    cout << 'array length ' << length << "test" << endl; 
+    // insertion_sort(array, length);
+}
+
+// int insertion_sort(int array[], int length) {
+//     cout << "sort array here\n";
+//     int sorted_array[length];
+
+//     for(int i = 1; i < length; i++) {
+//         int current = array[i];
+        
+//         int i2 = i - 1;
+//         while(i2 >= 0 && current < array[i2]) {
+//             int array[] = array[i]
+            
+//         }
+//     }
+// }
