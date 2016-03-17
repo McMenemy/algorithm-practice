102a862553b5af2e8bbeedf6036a77d92c9022cd
mcmenemy <mr_mcmenemy@yahoo.com>
2016-03-17 15:10:40 -0700

    improve runtime of selection_sort

--- ch2_getting_started/selection_sort.py
+++ ch2_getting_started/selection_sort.py
@@ -1,11 +1,22 @@
 def selection_sort(array):
     for i in range(0, len(array) - 1):
-        smallest = min(array[i:])
-        smallest_ind = array[i:].index(smallest) + i
+        smallest = find_min_and_ind(array[i:])
         temp = array[i]
-        array[smallest_ind] = temp
-        array[i] = smallest
+        array[smallest['ind'] + i] = temp
+        array[i] = smallest['num']
 
     return array
+
+def find_min_and_ind(array):
+    smallest = float('Inf')
+    smallest_ind = 0;
+    for i, num in enumerate(array):
+        if num < smallest:
+            smallest = num
+            smallest_ind = i
+
+    return { 'num': smallest, 'ind': smallest_ind }
+
+
 
 print selection_sort([4, 3, 2, 5, 2, 6])
