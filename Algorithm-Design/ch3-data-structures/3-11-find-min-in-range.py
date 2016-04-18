27f651085c0648c887d8cdf996c4e57794c693ac
mcmenemy <mr_mcmenemy@yahoo.com>
2016-04-18 13:26:48 -0700

    do part of problem 3-11 b: min in range

--- Algorithm-Design/ch3-data-structures/3-11-find-min-in-range.py
+++ Algorithm-Design/ch3-data-structures/3-11-find-min-in-range.py
@@ -27,3 +27,48 @@ class MinInRangeDict:
 # print(dictA.min_in_range(0, 0))
 # print(dictA.min_in_range(0, 4))
 # print(dictA.min_in_range(2, 4))
+
+# part b
+# a binary tree where the first node contains the lowest value in the whole array. The left child then contains the lowest value in the left half of the array and the right child contains the lowest value in the right half of the array. This continues recusively and the leafs then represent the lowest value at a particuluar location. This tree will have n + n/2 + n/4 + ... + 1 nodes. aka O(nlog(n))
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+    def set_left_child(self, node):
+        self.left_child = node
+
+    def set_right_child(self, node):
+        self.right_child = node
+
+class BetterMinRangeDict:
+    def __init__(self, array):
+        self.array = array
+        self.root = self.create_lookup_tree(array)
+
+    def create_lookup_tree(self, array):
+        if len(array) == 1:
+            return Node(array[0])
+        elif len(array) == 0:
+            return None
+
+        parent_node = Node(min(array))
+        mid = len(array) / 2
+        parent_node.left_child = self.create_lookup_tree(array[0: mid])
+        parent_node.right_child = self.create_lookup_tree(array[mid:])
+        return parent_node
+
+    def get_min_in_range(i, j):
+        i_counter = 0
+        j_counter = 0
+        # continue here
+
+dictB = BetterMinRangeDict([1, 6, 9, 3, 9, 4, 2])
+print(dictB.root.value)
+print(dictB.root.left_child.value)
+print(dictB.root.right_child.value)
+# print(dictB.min_in_range(0, 0))
+# print(dictB.min_in_range(0, 4))
+# print(dictB.min_in_range(2, 4))
