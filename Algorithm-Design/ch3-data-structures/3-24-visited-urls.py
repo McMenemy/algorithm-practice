e45cda3c770dce9e7c01fd3b0b5b756e5df3db94
mcmenemy <mr_mcmenemy@yahoo.com>
2016-05-02 21:14:03 -0700

    do problem 3-24 prefix tree

--- Algorithm-Design/ch3-data-structures/3-24-visited-urls.py
+++ Algorithm-Design/ch3-data-structures/3-24-visited-urls.py
@@ -1,7 +1,31 @@
 # use a prefix tree
 
 class Node:
-    def __init__(self, value=None,
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
 
 class PreFixTree:
     def __init__(self):
+        self.root = Node('', {})
+
+    def add(self, value):
+        current = self.root
+        i = 0
+        key = value[i]
+        while key in current.next:
+            current = current.next[key]
+            i += 1
+            key = value[i]
+
+        print(value[i:])
+        for char in value[i:]:
+            new_node = Node(char, {})
+            current.next[char] = new_node
+            current = new_node
+
+tree = PreFixTree()
+tree.add('www.google.com')
+tree.add('www.yahoo.com')
+tree.add('msn.com')
+tree.add('w.com')
