8dfe1da49ab107fc8d07bbe5d99c57d925cc70f4
McMenemy <mr_mcmenemy@yahoo.com>
2016-06-03 14:34:43 -0700

    4-13 heap questions

---
+++ Algorithm-Design/ch4-sorting-and-searching/4-13-heap-questions.py
@@ -0,0 +1,8 @@
+# a
+# max-heap because you can build in linear time and then find the max element in constant time, whereas sorting an array requires nlogn time to make (doesnt matter if you dont take into account construction time)
+# b
+# doesnt matter if you dont take into account construction time because in a sorted array you could bsearch to find the element to delets in lg(n) time and then delete it in n time. Whereas in a heap it would take n time to find the element and then lg(n) time to delete. If take into account construction time then a heap is better. and assuming index is know then the heap is better because you can delete in lgn time
+# c
+# a heap becasue it can be built in n time where as an sorted array takes n log n
+# d
+# a sorted array because you could find the min element in constant time whereas a max heap would take n time.
