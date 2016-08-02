1b0563ba41e4456ebb9d793ee3205f03f74da0ff
McMenemy <mr_mcmenemy@yahoo.com>
2016-08-02 13:12:29 -0700

    do problem 4-15a find second largest

--- Algorithm-Design/ch4-sorting-and-searching/4-15-second-largest-heap.py
+++ Algorithm-Design/ch4-sorting-and-searching/4-15-second-largest-heap.py
@@ -1,1 +1,18 @@
-do this
+# part a
+def find_second_largest(array):
+	largest = float("-inf")
+	second_largest = float("-inf")
+
+	for num in array:
+		if num >= largest:
+			second_largest = largest
+			largest = num
+		elif num > second_largest:
+			second_largest = num
+
+	return second_largest
+
+print(find_second_largest([3, 4, 1, 8, 9]))
+
+# part b
+# build a heap in linear time pg 115 book and then take the min element twice (lg(n)) time
